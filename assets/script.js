b76e228b7431ba04a7737c5366217fcc05772806
oolyster <adeline@reaux.me>
2026-09-11 12:48:03 +0200

    feat: ajout fleches et bullets

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,42 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+// appeler la div de class dots
+const dots = document.querySelector(".dots");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const bannerImage = document.querySelector(".banner-img");
+const bannerText = document.querySelector("#banner p");
+let dotsIndex = 0;
+
+const displayDots = () => {
+  dots.innerHTML = ""; // on vide la div de class dots
+  for (let i = 0; i < slides.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("dot");
+    dots.appendChild(div);
+
+    if (i === dotsIndex) {
+      div.classList.add("dot_selected");
+    }
+  }
+};
+
+displayDots();
